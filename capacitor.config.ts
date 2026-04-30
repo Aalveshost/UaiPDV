bdea9e80e77082920fade0f83425f9c6d05ee62b
Aalves <aalves.host@gmail.com>
2026-04-30 11:03:45 -0300

    fix: use json config and update to node 24/java 21

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.uaipdv.app',
-  appName: 'Uai PDV',
-  webDir: 'out'
-};
-
-export default config;
